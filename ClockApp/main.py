7e3ac136b00158bdd7cb987c50e10b24f4ad63f5
SpennyGee <49570419+SpennyGee@users.noreply.github.com>
2020-09-29 11:17:39 -0700

    Add files via upload

---
+++ ClockApp/main.py
@@ -0,0 +1,56 @@
+from kivy.app import App
+from kivy.core.text import LabelBase
+from kivy.core.window import Window
+from kivy.utils import get_color_from_hex
+import time
+from kivy.clock import Clock
+from kivy.properties import ObjectProperty
+from kivy.uix.boxlayout import BoxLayout
+    
+class ClockLayout(BoxLayout):
+    time_prop = ObjectProperty(None)
+    
+class ClockApp(App):
+    sw_seconds = 0
+    sw_started = False
+    
+    def update_time(self, nap):
+        if self.sw_started:
+            self.sw_seconds += nap
+     
+        self.root.ids.time.text = time.strftime('[b]%H[/b]:%M:%S')
+       
+        m, s = divmod(self.sw_seconds, 60)
+        self.root.ids.stopwatch.text = ('%02d:%02d.[size=40]%02d[/size]' %
+                                        (int(m), int(s), int(s * 100 % 100)))
+    def on_start(self):
+        Clock.schedule_interval(self.update_time, 0)
+        
+        
+    def start_stop(self):
+        self.root.ids.start_stop.text = ('Start'
+                                        if self.sw_started else 'Stop')
+        self.sw_started = not self.sw_started
+        
+    def reset(self):
+        if self.sw_started:
+            self.root.ids.start_stop.text = 'Start'
+            self.sw_started = False
+        self.sw_seconds = 0
+    #===========================================================================
+    # def on_start(self):
+    #     Clock.schedule_interval(self.update_time, 1)
+    #===========================================================================
+        
+if __name__ == "__main__":
+    
+    LabelBase.register(name='Roboto',
+                   fn_regular='Roboto-Thin.ttf',
+                   fn_bold='Roboto-Medium.ttf')
+    
+    Window.clearcolor = get_color_from_hex('#502938')
+   
+    ClockApp().run()
+
+
+
